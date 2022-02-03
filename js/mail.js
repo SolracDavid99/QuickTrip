458e8fa918536e05d44680efaa377754be4e6e74
SolracDavid99 <carlosdavidfregoso99@gmail.com>
2022-02-02 18:47:35 -0600

    Se agrega estilo al formulario en la deteccion de errores asi como un mensaje pop up

--- js/mail.js
+++ js/mail.js
@@ -15,17 +15,17 @@
 // }
 // )
 
-function sendMail(){
-    Email.send({
-        Host : "smtp.mailtrap.io",
-        Username : "ecd9369ddea454",
-        Password : "0ee3d7e5988e94",
-        To : 'quicktripcontact@gmail.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-        msg => alert("¡Gracias por contactarnos! En breve nos comunicaremos contigo. :)")
-    );
+// function sendMail(){
+//     Email.send({
+//         Host : "smtp.mailtrap.io",
+//         Username : "ecd9369ddea454",
+//         Password : "0ee3d7e5988e94",
+//         To : 'quicktripcontact@gmail.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//         msg => alert("¡Gracias por contactarnos! En breve nos comunicaremos contigo. :)")
+//     );
 
-}
+// }
