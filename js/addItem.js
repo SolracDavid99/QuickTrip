7535827e6b97f1c9ed2e2dbcd25004e18c30df70
SolracDavid99 <carlosdavidfregoso99@gmail.com>
2022-03-01 10:55:07 -0600

    Se modifica Form para agregar dos campos de imagenes , asi como se omiten los test de regex en nombre y rating

--- js/addItem.js
+++ js/addItem.js
@@ -1,10 +1,10 @@
 function agregarItem(){
 
-    const regexName = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g;
+    //const regexName = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g;
 
     const regexNumber = /^[0-9]*$/
 
-    const regexRating = /^[0-9]+[.]+[0-9]+[0-9]*$/
+    // const regexRating = /^[0-5]*$/
     
     var tourName = document.getElementById("inputTourName").value;
     var category = document.getElementById("formControlCategory").value;
@@ -12,10 +12,12 @@ function agregarItem(){
     var state = document.getElementById("inputState").value;
     var rating = document.getElementById("inputRating").value;
     var reviews= document.getElementById("inputReviews").value;
-    var imageUrl = document.getElementById("inputImageUrl").value;
+    var imageUrl1 = document.getElementById("inputImageUrl1").value;
+    var imageUrl2 = document.getElementById("inputImageUrl2").value;
+    var imageUrl3 = document.getElementById("inputImageUrl3").value;
     var description = document.getElementById("textAreaDescription").value;
 
-    console.log(tourName,category,price,state,rating,reviews,imageUrl,description);
+    // console.log(tourName,category,price,state,rating,reviews,imageUrl,description);
 
     document.getElementsByTagName("input")[0].classList.remove("borderColorDanger","borderColorAlert");
     document.getElementsByTagName("select")[0].classList.remove("borderColorDanger","borderColorAlert");
@@ -24,6 +26,8 @@ function agregarItem(){
     document.getElementsByTagName("input")[3].classList.remove("borderColorDanger","borderColorAlert");
     document.getElementsByTagName("input")[4].classList.remove("borderColorDanger","borderColorAlert");
     document.getElementsByTagName("input")[5].classList.remove("borderColorDanger","borderColorAlert");
+    document.getElementsByTagName("input")[6].classList.remove("borderColorDanger","borderColorAlert");
+    document.getElementsByTagName("input")[7].classList.remove("borderColorDanger","borderColorAlert");
     document.getElementsByTagName("textarea")[0].classList.remove("borderColorDanger","borderColorAlert");
 
     danger.style.display = 'none';
@@ -35,13 +39,13 @@ function agregarItem(){
     alert5.style.display = 'none';
 
     //test con regex
-    const onlyLettersTours = regexName.test(tourName);
+    //const onlyLettersTours = regexName.test(tourName);
     const onlyNumbersPrice = regexNumber.test(price);
-    const onlyLettersState = regexName.test(state);
-    const onlyRating = regexRating.test(rating);
+    // const onlyLettersState = regexName.test(state);
+    // const onlyRating = regexRating.test(rating);
     const onlyNumbersReviews = regexNumber.test(reviews);
 
-    if(tourName.length == 0 || price.length == 0|| state.length == 0 || rating.length == 0 || reviews.length == 0 || imageUrl.length == 0 || description.length == 0){
+    if(tourName.length == 0 || price.length == 0|| state.length == 0 || rating.length == 0 || reviews.length == 0 || imageUrl1.length == 0 || imageUrl2.length == 0 || imageUrl3.length == 0 || description.length == 0){
 
         danger.style.display = 'block';
 
@@ -60,35 +64,37 @@ function agregarItem(){
         if(reviews.length == 0 ){
             document.getElementById("inputReviews").classList.add("borderColorDanger");
         }
-        if(imageUrl.length == 0 ){
-            document.getElementById("inputImageUrl").classList.add("borderColorDanger");
+        if(imageUrl1.length == 0 ){
+            document.getElementById("inputImageUrl1").classList.add("borderColorDanger");
+        }
+        if(imageUrl2.length == 0 ){
+            document.getElementById("inputImageUrl2").classList.add("borderColorDanger");
+        }
+        if(imageUrl3.length == 0 ){
+            document.getElementById("inputImageUrl3").classList.add("borderColorDanger");
         }
         if(description.length == 0){
             document.getElementById("textAreaDescription").classList.add("borderColorDanger");
         }
 
-    }else if(onlyLettersTours==false){
-        alert1.style.display = 'block';
-        document.getElementById("inputTourName").classList.add("borderColorAlert");
     }
-    else if(onlyNumbersPrice==false){
+    // else if(onlyLettersTours==false){
+    //     alert1.style.display = 'block';
+    //     document.getElementById("inputTourName").classList.add("borderColorAlert");
+    // }
+    else if( onlyNumbersPrice==false ){
         alert2.style.display = 'block';
         document.getElementById("inputPrice").classList.add("borderColorAlert");
     }
-    // else if(onlyLettersState==false){
-    //     alert3.style.display = 'block';
-    //     document.getElementById("inputState").classList.add("borderColorAlert");
-    // }
-    else if(onlyRating==false){
+    else if( rating<0||rating>5 ){
         alert3.style.display = 'block';
         document.getElementById("inputRating").classList.add("borderColorAlert");
     }
-    else if(onlyNumbersReviews==false){
+    else if( onlyNumbersReviews==false ){
         alert3.style.display = 'block';
         document.getElementById("inputReviews").classList.add("borderColorAlert");
     }
     else{
-
     var tour = {
         tourName : tourName,
         category : category,
@@ -96,7 +102,9 @@ function agregarItem(){
         state : state , 
         rating : rating ,
         reviews : reviews ,
-        imageUrl : imageUrl ,
+        imageUrl1 : imageUrl1 ,
+        imageUrl2 : imageUrl1 ,
+        imageUrl3 : imageUrl1 ,
         description : description
     }
 
