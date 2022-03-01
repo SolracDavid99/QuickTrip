d62b581622e3b26de9fec64507269c7601adedad
AngelLagc <angelkirklagc@gmail.com>
2022-03-01 13:32:41 -0600

    Función calendario

---
+++ js/checkin.js
@@ -0,0 +1,9 @@
+/*Calendario*/
+
+function myFunction() { 
+    let date = new Date();
+    document.getElementById("myDate").min = date;
+}
+
+/*let output = String(date.getDate()).padStart(2, "0") + "/" + String(date.getMonth() + 1).padStart(2, "0") + "/" + date.getFullYear();*/
+
